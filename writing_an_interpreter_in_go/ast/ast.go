e875812b7a4954543d4603c58fd350ee99496e64
Will <williampayne23@gmail.com>
2024-10-12 22:46:17 +0100

    Return expressions writing_an_interpreter_in_go
    Probably will drop these later

--- writing_an_interpreter_in_go/ast/ast.go
+++ writing_an_interpreter_in_go/ast/ast.go
@@ -49,6 +49,10 @@ func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
 }
 
+type ReturnStatement struct {
+	Token       token.Token // the 'return' token
+	ReturnValue Expression
+}
 
-
-
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
--- writing_an_interpreter_in_go/parser/parser.go
+++ writing_an_interpreter_in_go/parser/parser.go
@@ -57,6 +57,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -79,6 +81,17 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	return stmt
 }
 
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+	p.nextToken()
+	// TODO: We're skipping the expressions until we
+	// encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
+}
+
 func (p *Parser) curTokenIs(t token.TokenType) bool {
 	return p.curToken.Type == t
 }
@@ -92,7 +105,7 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
-        p.peekError(t)
+		p.peekError(t)
 		return false
 	}
 }
