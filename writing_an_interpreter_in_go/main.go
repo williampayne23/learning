31074f0693a1e18af45169560e7eb8dea4bf48b1
Will <williampayne23@gmail.com>
2024-11-17 15:25:54 +0000

    Repl supports parser

--- writing_an_interpreter_in_go/main.go
+++ writing_an_interpreter_in_go/main.go
@@ -1,19 +1,52 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "os/user"
+	"fmt"
+	"github.com/urfave/cli/v2"
+	"log"
+	"os"
     "wl/repl"
 )
 
 func main() {
-    user, err := user.Current()
-    if err != nil {
-        panic(err)
-    }
+	app := &cli.App{
+		Commands: []*cli.Command{
+			{
+				Name:  "repl",
+				Usage: "Run the repl",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:  "step",
+						Value: "parser",
+						Usage: "The step to run",
+					},
+				},
+                Action: func(cCtx *cli.Context) error {
+                    fmt.Println("Running repl")
+                    var step = cCtx.String("step")
+                    if step == "lexer" {
+                        fmt.Println("Running lexer")
+                        repl.Start(os.Stdin, os.Stdout, repl.Lexer_eval)
+                    } else if step == "parser" {
+                        fmt.Println("Running parser")
+                        repl.Start(os.Stdin, os.Stdout, repl.Parser_eval)
+                    }
+                    return nil
+                },
+			},
+			{
+				Name:    "complete",
+				Aliases: []string{"c"},
+				Usage:   "complete a task on the list",
+				Action: func(cCtx *cli.Context) error {
+					fmt.Println("completed task: ", cCtx.Args().First())
+					return nil
+				},
+			},
+		},
+	}
 
-    fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
-    fmt.Printf("Feel free to type in commands\n")
-    repl.Start(os.Stdin, os.Stdout)
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
--- writing_an_interpreter_in_go/repl/repl.go
+++ writing_an_interpreter_in_go/repl/repl.go
@@ -5,12 +5,15 @@ import (
     "fmt"
     "io"
     "wl/lexer"
+    "wl/parser"
     "wl/token"
 )
 
 const PROMPT = ">> "
 
-func Start(in io.Reader, out io.Writer) {
+type EvalFunc func(string) string
+
+func Start(in io.Reader, out io.Writer, eval EvalFunc) {
     scanner := bufio.NewScanner(in)
 
     for {
@@ -21,10 +24,37 @@ func Start(in io.Reader, out io.Writer) {
         }
 
         line := scanner.Text()
-        l := lexer.New(line)
-
-        for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-            fmt.Printf("%+v\n", tok)
+        evaluated := eval(line)
+        if evaluated != "" {
+            fmt.Println(evaluated)
         }
     }
+}
+
+
+func Lexer_eval(input string) string {
+    l := lexer.New(input)
+
+    for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+        fmt.Printf("%+v\n", tok)
+    }
+
+    return ""
+}
+
+
+func Parser_eval(input string) string {
+    l := lexer.New(input)
+    p := parser.New(l)
+
+    program := p.ParseProgram()
+    if len(p.Errors()) != 0 {
+        fmt.Println("Parser has errors:")
+        for _, msg := range p.Errors() {
+            fmt.Println("\t" + msg)
+        }
+        return ""
+    }
+
+    return program.String()
 }
