bda0b4b73afc49f3c533529cd217aabc90b9aecc
Will Payne <willpayne@Wills-MacBook-Pro.local>
2025-11-29 14:12:15 +0000

    WIP

--- bevy_clicker/src/ui/mod.rs
+++ bevy_clicker/src/ui/mod.rs
@@ -5,14 +5,18 @@ use haalka::prelude::*;
 
 pub mod components;
 
+/// Only sets up dependencies for UI functionality.
+/// Actually spawning the UI elements is for other plugins to handle.
+/// This library should just export builders
 pub struct UIPlugin;
 
 impl Plugin for UIPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(HaalkaPlugin).add_systems(Startup, setup_ui);
+        app.add_plugins(HaalkaPlugin);
+        //.add_systems(Startup, setup_ui);
     }
 }
 
-fn setup_ui(world: &mut World) {
-    button::ui_root().spawn(world);
-}
+// fn setup_ui(world: &mut World) {
+//     button::ui_root().spawn(world);
+// }
